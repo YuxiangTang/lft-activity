df4e9459bf270027e6d48e48a45a43366b7ed557
littlegirlRachel <15033592279@163.com>
2018-02-05 22:52:49 +0800

    修复页面缩放问题，精准定位

--- lft_js/nav-side.js
+++ lft_js/nav-side.js
@@ -10,19 +10,15 @@ $(window).scroll(function () {
     $(".slider ul li").css("background-color", "#fff100");
     $(".slider ul li:eq(" + n + ")").css("background-color", "#fff");
     $(".slider ul li:eq(5)").css("background-color", "transparent");
-
 }
-
-
 );
 
 $(function () {
-    $(".slider ul li").bind('click',function() {
+    $(".slider ul li").click(function() {
         var index = $(".slider ul li").index(this);
-        var wrapH1 = $(".l-links-content").height();
-        console.log("wrapH1="+wrapH1);
         if (!(index >= 5)) {
-            $("html,body").animate({scrollTop: index * 1000 + 700}, 800);
+            var wrapH1 = $(".l-links-content:eq(" + index + ")").height();
+            $("html,body").animate({scrollTop: (wrapH1+150)*(index+1)-600}, 800);
             $(".slider ul li").css({"background-color": "#fff100"});
             $(".slider ul li:eq(" + index + ")").css("background-color", "#fff");
 
